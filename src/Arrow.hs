f6840a6cf41fbb305c7dd0556717b6bc619b8521
Spencer Hubbard <hubbardspencerm@gmail.com>
2019-09-25 14:03:54 -0700

    Document Arrow laws

--- src/Arrow.hs
+++ src/Arrow.hs
@@ -1,6 +1,7 @@
 -- | This module contains a type class for arrows.
 module Arrow (
-    Arrow (..)
+    assoc
+  , Arrow (..)
   , second
   , (***)
   , (&&&)
@@ -10,9 +11,42 @@ module Arrow (
 
 import Data.Tuple ( swap )
 
+assoc :: ((a, b), c) -> (a, (b, c))
+assoc ((x, y), z) = (x, (y, z))
+
 -- | Type class for arrows.
 --
--- TODO: add arrow laws
+-- Identity laws:
+--
+-- > arr id >>> f = f
+--
+-- > f >>> arr id = f
+--
+-- Associativity:
+--
+-- > (f >>> g) >>> h = f >>> (g >>> h)
+--
+-- Distributivity of `arr` over `>>>`:
+--
+-- > arr (f >>> g) = arr f >>> arr g
+--
+-- Distributivity of `first` over `>>>`:
+--
+-- > first (f >>> g) = first f >>> first g
+--
+-- Definition of `first`:
+--
+-- > first f >>> arr fst = arr fst >>> f
+--
+-- > first f >>> second (arr g) = second (arr g) >>> first f
+--
+-- > arr assoc >>> first f = first (first f) >>> arr assoc
+--
+-- Commutivity of `first` and `arr`:
+--
+-- NOTE: the following law seems to only apply to the `->` instance.
+--
+-- > first (arr f) = arr (first f)
 --
 class Arrow t where
   -- lift a function into an arrow
@@ -43,6 +77,13 @@ liftA2 op f g = (f &&& g) >>> arr (\ (x, y) -> x `op` y)
 
 -- -----------------------------------------------------------------------------
 -- Example instances
+
+instance Arrow (->) where
+  arr f = f
+  -- arr = id
+  f >>> g = g . f
+  -- (>>>) = flip (.)
+  first f (x, y) = (f x, y)
 
 data Kleisli m a b = K (a -> m b)
 
